7933c5a2d32f29502f155053bae531a64b0f2c45
Mahajan, Divya (01S) <divya.mahajan@fmr.com>
2018-10-13 15:20:58 -0400

    adding email component

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+      "es6": true,
+      "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017
+},
+
+  "extends": "eslint:recommended",
+  "rules": {
+      "indent": [
+          "error",
+          "tab"
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ],
+      "no-console": 0
+  }
+};
---
+++ components/email/send.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+ 
+const ses = new AWS.SES()
+const myEmail = process.env.EMAIL
+const myDomain = process.env.DOMAIN
+
+function generateResponse (code, payload) {
+	return {
+		statusCode: code,
+		headers: {
+			'Access-Control-Allow-Origin': myDomain,
+			'Access-Control-Allow-Headers': 'x-requested-with',
+			'Access-Control-Allow-Credentials': true
+		},
+		body: JSON.stringify(payload)
+	}
+}
+
+function generateError (code, err) {
+	console.log(err)
+	return {
+		statusCode: code,
+		headers: {
+			'Access-Control-Allow-Origin': myDomain,
+			'Access-Control-Allow-Headers': 'x-requested-with',
+			'Access-Control-Allow-Credentials': true
+		},
+		body: JSON.stringify(err.message)
+	}
+}
+
+function generateEmailParams (body) {
+	const { firstName, lastName, content, email } = JSON.parse(body)
+	console.log( firstName, lastName, content, email )
+	if (!(firstName && lastName && content && email)) {
+		throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'content\'.')
+	}
+
+	return {
+		Source: myEmail,
+		Destination: { ToAddresses: [email] },
+		ReplyToAddresses: [email],
+		Message: {
+			Body: {
+				Text: {
+					Charset: 'UTF-8',
+					Data: `New Message from ItAllAddsUp by ${firstName}  ${lastName} \nContent: ${content}`
+				}
+			},
+			Subject: {
+				Charset: 'UTF-8',
+				// Data: `You received a message from ${myDomain}!`
+				Data: 'You received a message from ItAllAddsUp!'
+			}
+		}
+	}
+}
+
+module.exports.send = async (event) => {
+	try {
+		console.log('email body', JSON.parse(event.body))
+		const emailParams = generateEmailParams(event.body)
+		const data = await ses.sendEmail(emailParams).promise()
+		return generateResponse(200, data)
+	} catch (err) {
+		return generateError(500, err)
+	}
+}
