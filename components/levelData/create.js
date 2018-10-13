4dbe90895d0efa7fc9af889c3a0856623f6f37b3
Mahajan, Divya (01S) <divya.mahajan@fmr.com>
2018-10-13 16:28:17 -0400

    adding level data component

---
+++ components/levelData/create.js
@@ -0,0 +1,80 @@
+'use strict'
+
+const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient() // todo
+
+function validateInput (event) {
+	const req = JSON.parse((event.body))
+	const pointsRange = req[0].pointsRange
+	let bool = true 
+	if (pointsRange === undefined) {
+		console.log('pointsRange is undefined')
+		bool = false
+	}
+	return bool
+}
+
+module.exports.create = (event, context, callback) => {
+	const timestamp = new Date().getTime()
+	const dataBody = JSON.parse(event.body)
+	console.log(`incoming payload${  dataBody}`)
+	if (!validateInput(event)) {
+		console.error('Validation Failed')
+		callback(null, {
+			statusCode: 400,
+			headers: { 'Content-Type': 'text/plain' },
+			body: 'Couldn\'t create the level data item.'
+		})
+		return
+	}
+
+	const createItemsArray = []
+	for(let i=0; i<dataBody.length; i+=1){
+		const item = {
+			PutRequest : {
+				Item : {
+					'pointsRange' : dataBody[i].pointsRange,
+					'statusGraphicUrl' : dataBody[i].statusGraphicUrl,
+					'status' : dataBody[i].status,
+					'createdDate': timestamp,
+					'updatedAt': timestamp
+
+				}
+			}
+		}
+		createItemsArray.push(item)
+	}
+
+	const params = {
+		RequestItems : {
+			'serverless-backend-levelData' : createItemsArray
+		}
+	}
+
+	// write the performActions to the database
+	dynamoDb.batchWrite(params, (err, data) => {
+		// handle potential errors
+		if (err) {
+			console.log('Batch create unsuccessful ...')
+			console.error(err, err.stack)
+			callback(null, {
+				statusCode: err.statusCode || 501,
+				headers: { 'Content-Type': 'text/plain' },
+				body: 'Couldn\'t create the adminData item.'
+			})
+			return
+		} 
+		console.log('Batch create successful ...')
+		console.log(data)
+		console.log('Logging any unprocessed records ...', data.UnprocessedItems)
+    
+
+		// create a response
+		const response = {
+			statusCode: 200,
+			body: (data)
+		}
+		callback(null, response)
+	})
+}
---
+++ components/levelData/list.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.list = (event, context, callback) => {
+	const params = {
+		TableName: process.env.LEVEL_DATA_DYNAMODB_TABLE
+	}
+
+	// fetch action from the database
+	dynamoDb.scan(params, (error, result) => {
+		// handle potential errors
+		if (error) {
+			console.error(error)
+			callback(null, {
+				statusCode: error.statusCode || 501,
+				headers: { 'Content-Type': 'text/plain' },
+				body: 'Couldn\'t fetch the level items.'
+			})
+			return
+		}
+
+		console.log('returned resultset', JSON.stringify(result.Items))
+
+		// create a response
+		const response = {
+			statusCode: 200,
+			body: (result.Items)
+		}
+		callback(null, response)
+	})
+}
