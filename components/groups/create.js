6e4bd554e8d46a790e579e1b966fb685d90df83b
Mahajan, Divya (01S) <divya.mahajan@fmr.com>
2018-10-13 18:37:20 -0400

    modify members delimiter to space

--- components/groups/create.js
+++ components/groups/create.js
@@ -5,89 +5,89 @@ const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-depen
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
 function validateInput (event) {
-  // const req = JSON.parse((event.body));
-  // const { name } = req;
-  // let bool = true; 
-  //   if (name === undefined) {
-  //     console.log('name is undefined');
-  //     bool = false;
-  //  }
-  //  return bool;
-  return event;
- }
-
-module.exports.create = (event, context, callback) => {
-  const timestamp = new Date().getTime()
-  const dataBody = JSON.parse(event.body)
-  console.log(`incoming payload${  dataBody}`)
-  if (!validateInput(event)) {
-    console.error('Validation Failed')
-    callback(null, {
-      statusCode: 400,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Couldn\'t create the group item.'
-    })
-    return
-  }
-
-  const createGroupsArray = [];
-  for(let i=0; i<dataBody.length; i+=1){
-    // const arr = [];
-    let members = [];
-    members = dataBody[i].members.split(',');
-    console.log("membersArray", members);   
-    for(let abc=0; abc < members.length; abc+=1){
-      // arr.push({name:members[i]});
-      const item = {
-      PutRequest : {
-          Item : {
-          'name': dataBody[i].name,
-          'leader': dataBody[i].username,
-          'members': members[abc],
-          'pointsEarned': dataBody[i].pointsEarned,
-          'zipCode': dataBody[i].zipCode,
-          'groupType': dataBody[i].groupType,
-          'groupSubType': dataBody[i].groupSubType,
-          'description': dataBody[i].description,
-          'groupAvatar': dataBody[i].groupAvatar,
-          'createdDate': timestamp,
-          'updatedAt': timestamp
-
-           }
-        }
-      };
-    createGroupsArray.push(item);
-  }
+	// const req = JSON.parse((event.body));
+	// const { name } = req;
+	// let bool = true; 
+	//   if (name === undefined) {
+	//     console.log('name is undefined');
+	//     bool = false;
+	//  }
+	//  return bool;
+	return event
 }
 
-  const params = {
-    RequestItems : {
-      'cis-serverless-backend-groups' : createGroupsArray
-    }
-  };
+module.exports.create = (event, context, callback) => {
+	const timestamp = new Date().getTime()
+	const dataBody = JSON.parse(event.body)
+	console.log(`incoming payload${  dataBody}`)
+	if (!validateInput(event)) {
+		console.error('Validation Failed')
+		callback(null, {
+			statusCode: 400,
+			headers: { 'Content-Type': 'text/plain' },
+			body: 'Couldn\'t create the group item.'
+		})
+		return
+	}
 
-  // write the todo to the database
-  dynamoDb.batchWrite(params, (err, data) => {
-    // handle potential errors
-    if (err) {
-      console.log('Batch create unsuccessful ...');
-      console.error(err, err.stack);
-      callback(null, {
-        statusCode: err.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the group item.'
-      })
-      return
-    } 
-      console.log('Batch create successful ...');
-      console.log(data);
-      console.log('Logging any unprocessed records ...', data.UnprocessedItems);
+	const createGroupsArray = []
+	for(let i=0; i<dataBody.length; i+=1){
+		// const arr = [];
+		let members = []
+		members = dataBody[i].members.split(' ')
+		console.log('membersArray', members)   
+		for(let abc=0; abc < members.length; abc+=1){
+			// arr.push({name:members[i]});
+			const item = {
+				PutRequest : {
+					Item : {
+						'name': dataBody[i].name,
+						'leader': dataBody[i].username,
+						'members': members[abc],
+						'pointsEarned': dataBody[i].pointsEarned,
+						'zipCode': dataBody[i].zipCode,
+						'groupType': dataBody[i].groupType,
+						'groupSubType': dataBody[i].groupSubType,
+						'description': dataBody[i].description,
+						'groupAvatar': dataBody[i].groupAvatar,
+						'createdDate': timestamp,
+						'updatedAt': timestamp
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: (data)
-    }
-    callback(null, response)
-  })
+					}
+				}
+			}
+			createGroupsArray.push(item)
+		}
+	}
+
+	const params = {
+		RequestItems : {
+			'cis-serverless-backend-groups' : createGroupsArray
+		}
+	}
+
+	// write the todo to the database
+	dynamoDb.batchWrite(params, (err, data) => {
+		// handle potential errors
+		if (err) {
+			console.log('Batch create unsuccessful ...')
+			console.error(err, err.stack)
+			callback(null, {
+				statusCode: err.statusCode || 501,
+				headers: { 'Content-Type': 'text/plain' },
+				body: 'Couldn\'t create the group item.'
+			})
+			return
+		} 
+		console.log('Batch create successful ...')
+		console.log(data)
+		console.log('Logging any unprocessed records ...', data.UnprocessedItems)
+
+		// create a response
+		const response = {
+			statusCode: 200,
+			body: (data)
+		}
+		callback(null, response)
+	})
 }
--- components/groups/delete.js
+++ components/groups/delete.js
@@ -4,32 +4,76 @@ const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-depen
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
+function validateInput (event) {
+	const req = JSON.parse((event.body))       
+	const { name, members } = req
+	console.log('name is:', name)
+	console.log('members are:', members)
+	let bool = true 
+	if (name === undefined && members === undefined) {
+		console.log('name and members are undefined')
+		bool = false
+	}
+	return bool
+}
+
 module.exports.delete = (event, context, callback) => {
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE_GROUPS,
-    Key: {
-      groupName: event.pathParameters.groupName
-    }
-  }
+	const dataBody = JSON.parse(event.body)
+	console.log(`incoming payload: ${dataBody}`)
+	if (!validateInput(event)) {
+		console.error('Validation Failed')
+		callback(null, {
+			statusCode: 400,
+			headers: { 'Content-Type': 'text/plain' },
+			body: 'Couldn\'t delete the group item(s).'
+		})
+		return
+	}
 
-  // delete the action from the database
-  dynamoDb.delete(params, (error) => {
-    // handle potential errors
-    if (error) {
-      console.error(error)
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t remove the group item.'
-      })
-      return
-    }
+	const deleteItemsArray = []
+	console.log('membersArray', dataBody.members)   
+	let members = dataBody.members
+	for(let abc=0; abc < members.length; abc+=1){
+		// arr.push({name:members[i]});
+		const item = {
+			DeleteRequest : {
+				Key : {
+					'name': dataBody.name
+					// 'members': members[abc]
+				}
+			}
+		}
+		deleteItemsArray.push(item)
+	}
 
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({})
-    }
-    callback(null, response)
-  })
+	const params = {
+		RequestItems : {
+			'cis-serverless-backend-groups' : deleteItemsArray
+		}
+	}
+
+	// write the todo to the database
+	dynamoDb.batchWrite(params, (err, data) => {
+		// handle potential errors
+		if (err) {
+			console.log('Batch create unsuccessful ...')
+			console.error(err, err.stack)
+			callback(null, {
+				statusCode: err.statusCode || 501,
+				headers: { 'Content-Type': 'text/plain' },
+				body: 'Couldn\'t delete the group item.'
+			})
+			return
+		} 
+		console.log('Batch create successful ...')
+		console.log(data)
+		console.log('Logging any unprocessed records ...', data.UnprocessedItems)
+
+		// create a response
+		const response = {
+			statusCode: 200,
+			body: (data)
+		}
+		callback(null, response)
+	})
 }
